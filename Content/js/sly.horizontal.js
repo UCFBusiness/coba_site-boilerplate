a5174157e3640518e88706220df062dd02e41dbe
Mo Henriquez <moises.henriquez@ucf.edu>
2013-10-10 18:35:57 -0400

    EOD Updates
    lots of script changes and layout fixes

---
+++ Content/js/sly.horizontal.js
@@ -0,0 +1,127 @@
+jQuery(function($){
+	'use strict';
+
+	// -------------------------------------------------------------
+	//   Centered Navigation
+	// -------------------------------------------------------------
+	(function () {
+		var $frame = $('#news');
+		var $section  = $frame.parent();
+
+		// Call Sly on frame
+		$frame.sly({
+			horizontal: 1,
+			itemNav: 'centered',
+			smart: 1,
+			activateOn: 'click',
+			mouseDragging: 1,
+			touchDragging: 1,
+			releaseSwing: 1,
+			startAt: 4,
+			scrollBar: $section.find('.scrollbar'),
+			scrollBy: 1,
+			speed: 300,
+			elasticBounds: 1,
+			easing: 'easeOutExpo',
+			dragHandle: 1,
+			dynamicHandle: 1,
+			clickBar: 1,
+
+			// Buttons
+			prev: $section.find('.prev'),
+			next: $section.find('.next')
+		});
+	}());
+
+    (function () {
+		var $frame = $('#events');
+		var $section  = $frame.parent();
+
+		// Call Sly on frame
+		$frame.sly({
+			horizontal: 1,
+			itemNav: 'centered',
+			smart: 1,
+			activateOn: 'click',
+			mouseDragging: 1,
+			touchDragging: 1,
+			releaseSwing: 1,
+			startAt: 4,
+			scrollBar: $section.find('.scrollbar'),
+			scrollBy: 1,
+			speed: 300,
+			elasticBounds: 1,
+			easing: 'easeOutExpo',
+			dragHandle: 1,
+			dynamicHandle: 1,
+			clickBar: 1,
+
+			// Buttons
+			prev: $section.find('.prev'),
+			next: $section.find('.next')
+		});
+	}());
+
+    (function () {
+		var $frame = $('#faculty');
+		var $section  = $frame.parent();
+
+		// Call Sly on frame
+		$frame.sly({
+			horizontal: 1,
+			itemNav: 'centered',
+			smart: 1,
+			activateOn: 'click',
+			mouseDragging: 1,
+			touchDragging: 1,
+			releaseSwing: 1,
+			startAt: 4,
+			scrollBar: $section.find('.scrollbar'),
+			scrollBy: 1,
+			speed: 300,
+			elasticBounds: 1,
+			easing: 'easeOutExpo',
+			dragHandle: 1,
+			dynamicHandle: 1,
+			clickBar: 1,
+
+			// Buttons
+			prev: $section.find('.prev'),
+			next: $section.find('.next')
+		});
+	}());
+
+	// -------------------------------------------------------------
+	//   Force Centered Navigation
+	// -------------------------------------------------------------
+	(function () {
+		var $frame = $('#none');
+		var $wrap  = $frame.parent();
+
+		// Call Sly on frame
+		$frame.sly({
+			horizontal: 1,
+			itemNav: 'forceCentered',
+			smart: 1,
+			activateMiddle: 1,
+			activateOn: 'click',
+			mouseDragging: 1,
+			touchDragging: 1,
+			releaseSwing: 1,
+			startAt: 0,
+			scrollBar: $wrap.find('.scrollbar'),
+			scrollBy: 1,
+			speed: 300,
+			elasticBounds: 1,
+			easing: 'easeOutExpo',
+			dragHandle: 1,
+			dynamicHandle: 1,
+			clickBar: 1,
+
+			// Buttons
+			prev: $wrap.find('.prev'),
+			next: $wrap.find('.next')
+		});
+	}());
+
+});
--- Scripts/main.js
+++ Scripts/main.js
@@ -10,143 +10,140 @@ require.config({
         'waypoint': 'js/waypoints.min',
         'easing': 'js/jquery.easing.min',
         'equalh': 'js/jquery.equalheight',
-        'sticky': 'js/jquery.sticky-kit.min',
         'resize': 'js/jquery.debouncedresize',
+        'lockfix': 'js/jquery.lockfixed.min',
         //scripts
         'sly-main': 'js/sly.main'
     }
 });
 
-require(['fittext', 'easing', 'sly', 'sly-main', 'dmenu'], function () {
+require(['fittext', 'easing', 'sly', 'sly-main', 'dmenu', 'lockfix'], function () {
 
-    // http://stackoverflow.com/questions/3698200/window-onload-vs-document-ready
-    // http://4loc.wordpress.com/2009/04/28/documentready-vs-windowload/
-
-    //---------------------------------------------------
-    // On PageReady...
-    // occurs as early as possible after the HTML document has loaded.
-    //---------------------------------------------------
-
-    $(document).ready(function () {
-
-        //----- Font Sizes-------------------------------
-        $('body').fitText(1.2, { minFontSize: '12px', maxFontSize: '16px' });
-        //-----------------------------------------------
+    //----- Font Sizes-------------------------------
+    $('body').fitText(1.2, { minFontSize: '12px', maxFontSize: '16px' });
+    //-----------------------------------------------
 
 
-        //-- navigation menu -----------------------------------
-        $('.dl-menuwrapper').dlmenu({ 
-            animationClasses: { 
-                classin: 'dl-animate-in-2', 
-                classout: 'dl-animate-out-2'
-            } 
-        });
-        //------------------------------------------------------
+    //-- navigation menu -----------------------------------
+    $('.dl-menuwrapper').dlmenu({ 
+        animationClasses: { 
+            classin: 'dl-animate-in-2', 
+            classout: 'dl-animate-out-2'
+        } 
+    });
+    //------------------------------------------------------
 
 
-        //----- fade out fixed menu----------------------
-        var nav = $('#nav');
+    //----- fade out fixed menu----------------------
+    var nav = $('#nav');
 
-        $(window).scroll(function () {
+    $(window).scroll(function () {
+        var scrollTop = $(window).scrollTop();
+        if (scrollTop != 0)
+            $(nav).stop().animate({ 'opacity': '0.2' }, 400);
+        else
+            $(nav).stop().animate({ 'opacity': '1' }, 400);
+    });
+
+    $(nav).hover(
+        function (e) {
             var scrollTop = $(window).scrollTop();
-            if (scrollTop != 0)
-                $(nav).stop().animate({ 'opacity': '0.2' }, 400);
-            else
-                $(nav).stop().animate({ 'opacity': '1' }, 400);
-        });
-
-        $(nav).hover(
-            function (e) {
-                var scrollTop = $(window).scrollTop();
-                if (scrollTop != 0) {
-                    $(this).stop().animate({ 'opacity': '1' }, 400);
-                }
-            },
-            function (e) {
-                var scrollTop = $(window).scrollTop();
-                if (scrollTop != 0) {
-                    $(this).stop().animate({ 'opacity': '0.2' }, 400);
-                }
+            if (scrollTop != 0) {
+                $(this).stop().animate({ 'opacity': '1' }, 400);
             }
-        );
-        //-----------------------------------------------
-
-
-        //-- lazy load images ---------------------------
-        $("img.lz").fadeTo(0, 0);
-        $(window).scroll(function (d, h) {
-            $('img.lz').each(function (i) {
-                a = $(this).offset().top + $(this).height() - 400;
-                b = $(window).scrollTop() + $(window).height();
-                if (a < b) $(this).fadeTo(500, 1);
-            })
-        }); //--------------------------------------------
-
-        //-- disable right-click ------------------------
-        $(document).bind("contextmenu", function (e) {
-            return false;
-            //-------------------------------------------
-        }); //--------------------------------------------
-
-        //-- check if cookies are enabled ---------------
-        var dt = new Date();
-        dt.setSeconds(dt.getSeconds() + 60);
-        document.cookie = "cookietest=1; expires=" + dt.toGMTString();
-        var cookiesEnabled = document.cookie.indexOf("cookietest=") != -1;
-        if (!cookiesEnabled) {
-            //cookies are not enabled
-            alert('cookies are not enabled');
-        } //----------------------------------------------
-
-
-        //-- back to top --------------------------------
-        var offset = 220;
-        var duration = 500;
-        var topButton = $('.back-to-top');
-
-        $(window).scroll(function () {
-            if ($(this).scrollTop() > offset) {
-                $(topButton).fadeIn(duration);
-            } else {
-                $(topButton).fadeOut(duration);
+        },
+        function (e) {
+            var scrollTop = $(window).scrollTop();
+            if (scrollTop != 0) {
+                $(this).stop().animate({ 'opacity': '0.2' }, 400);
             }
+        }
+    );
+    //-----------------------------------------------
+
+
+    //-- lazy load images ---------------------------
+    $("img.lz").fadeTo(0, 0);
+    $(window).scroll(function (d, h) {
+        $('img.lz').each(function (i) {
+            a = $(this).offset().top + $(this).height() - 400;
+            b = $(window).scrollTop() + $(window).height();
+            if (a < b) $(this).fadeTo(500, 1);
+        })
+    }); //--------------------------------------------
+
+
+    //-- easing -------------------------------------------
+    $('ul.menu a').bind('click', function (event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });//--------------------------------------------
+
+
+    //-- disable right-click ------------------------
+    $(document).bind("contextmenu", function (e) {
+        return false;
+        //-------------------------------------------
+    }); //--------------------------------------------
+
+
+    //-- check if cookies are enabled ---------------
+    var dt = new Date();
+    dt.setSeconds(dt.getSeconds() + 60);
+    document.cookie = "cookietest=1; expires=" + dt.toGMTString();
+    var cookiesEnabled = document.cookie.indexOf("cookietest=") != -1;
+    if (!cookiesEnabled) {
+        //cookies are not enabled
+        alert('cookies are not enabled');
+    } //----------------------------------------------
+
+
+    //-- back to top --------------------------------
+    var offset = 220;
+    var duration = 500;
+    var topButton = $('.back-to-top');
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > offset) {
+            $(topButton).fadeIn(duration);
+        } else {
+            $(topButton).fadeOut(duration);
+        }
+    });
+
+    $(topButton).click(function (event) {
+        event.preventDefault();
+        $('html, body').animate({ scrollTop: 0 }, duration);
+        return false;
+    });
+    //-----------------------------------------------
+
+
+    //-- global variables (not encouraged) ----------
+    var width = parseInt($(this).width());
+    var height = parseInt($(this).height());
+
+    if (width < 481) //load mobile scripts
+        require([], function () {
+            //-- screensize -------------------------
+            $("#message").html(width + " x " + height + " - mobile");
         });
 
-        $(topButton).click(function (event) {
-            event.preventDefault();
-            $('html, body').animate({ scrollTop: 0 }, duration);
-            return false;
+    if ((width > 480) && (width < 1025)) //load tablet scripts
+        require([], function () {
+            //-- screensize -------------------------
+            $("#message").html(width + " x " + height + " - tablet");
         });
-        //-----------------------------------------------
 
-    }); //end ready
+    if (width > 1024) //load desktop scripts
+        require([], function () {
+            //-- screensize -------------------------
+            $("#message").html(width + " x " + height + " - desktop");
+        });
 
-
-    $(document).ready(function () {
-
-        //-- global variables (not encouraged) ----------
-        var width = parseInt($(this).width());
-        var height = parseInt($(this).height());
-
-        if (width < 481) //load mobile scripts
-            require([], function () {
-                //-- screensize -------------------------
-                $("#message").html(width + " x " + height + " - mobile");
-            });
-
-        if ((width > 480) && (width < 1025)) //load tablet scripts
-            require([], function () {
-                //-- screensize -------------------------
-                $("#message").html(width + " x " + height + " - tablet");
-            });
-
-        if (width > 1024) //load desktop scripts
-            require([], function () {
-                //-- screensize -------------------------
-                $("#message").html(width + " x " + height + " - desktop");
-            });
-
-    }); //end ready
 
 
     //---------------------------------------------------
