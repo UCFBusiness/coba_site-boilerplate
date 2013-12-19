54742fa632bb3557897a889fd0eeb4039dfecd60
Mo Henriquez <moises.henriquez@ucf.edu>
2013-12-19 17:30:06 -0500

    EOD Updates
    content updates and styling fixes

---
+++ App_Code/wpsinglepostreader.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text; //+
+using System.Web;
+using System.Xml; //+
+
+/// <summary>
+/// Summary description for wpsinglepostreader
+/// </summary>
+public class wpsinglepostreader
+{
+    public wpsinglepostreader()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+}
