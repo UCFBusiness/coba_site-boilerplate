eba7cbe17cbbc63f3f67e1d1f24bb10a2efab09d
Mo Henriquez <moses.henriquez@gmail.com>
2014-07-15 17:44:22 -0400

    style fixes and updates to cover

--- Scripts/pages/home.js
+++ Scripts/pages/home.js
@@ -18,7 +18,7 @@ define(['jquery', 'domReady!', 'debounced', 'lazyloader', 'easing', 'sly', 'page
             //    //return screenheight - topBar;
             //    return screenheight;
             //});
-            
+
             // Lazyload website images
             //$('.img-responsive').bttrlazyloading({
             //    //placeholder: 'data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==',
@@ -27,13 +27,13 @@ define(['jquery', 'domReady!', 'debounced', 'lazyloader', 'easing', 'sly', 'page
             //    container: '.scroll-pane'
             //});
 
-            
+
 
             // Sly horizontal scroll
-            (function(){var $frame=$('#row1');var $section=$frame.parent();$frame.sly({horizontal:1,itemNav:'centered',smart:1,activateOn:'click',mouseDragging:1,touchDragging:1,releaseSwing:1,startAt:0,scrollBar:$section.find('.scrollbar'),scrollBy:1,pagesBar:$section.find('.pages'),activatePageOn:'click',speed:300,elasticBounds:1,easing:'easeOutExpo',dragHandle:1,dynamicHandle:1,clickBar:1,prev:$section.find('.prev'),next:$section.find('.next'),prevPage:$section.find('.prevPage'),nextPage:$section.find('.nextPage'),forward:$section.find('.forward'),backward:$section.find('.backward')});}());
-            (function(){var $frame=$('#row2');var $section=$frame.parent();$frame.sly({horizontal:1,itemNav:'centered',smart:1,activateOn:'click',mouseDragging:1,touchDragging:1,releaseSwing:1,startAt:0,scrollBar:$section.find('.scrollbar'),scrollBy:1,pagesBar:$section.find('.pages'),activatePageOn:'click',speed:300,elasticBounds:1,easing:'easeOutExpo',dragHandle:1,dynamicHandle:1,clickBar:1,prev:$section.find('.prev'),next:$section.find('.next'),prevPage:$section.find('.prevPage'),nextPage:$section.find('.nextPage'),forward:$section.find('.forward'),backward:$section.find('.backward')});}());
-            (function(){var $frame=$('#row3');var $section=$frame.parent();$frame.sly({horizontal:1,itemNav:'centered',smart:1,activateOn:'click',mouseDragging:1,touchDragging:1,releaseSwing:1,startAt:0,scrollBar:$section.find('.scrollbar'),scrollBy:1,pagesBar:$section.find('.pages'),activatePageOn:'click',speed:300,elasticBounds:1,easing:'easeOutExpo',dragHandle:1,dynamicHandle:1,clickBar:1,prev:$section.find('.prev'),next:$section.find('.next'),prevPage:$section.find('.prevPage'),nextPage:$section.find('.nextPage'),forward:$section.find('.forward'),backward:$section.find('.backward')});}());
-            
+            (function () { var $frame = $('#row1'); var $section = $frame.parent(); $frame.sly({ horizontal: 1, itemNav: 'centered', smart: 1, activateOn: 'click', mouseDragging: 1, touchDragging: 1, releaseSwing: 1, startAt: 0, scrollBar: $section.find('.scrollbar'), scrollBy: 1, pagesBar: $section.find('.pages'), activatePageOn: 'click', speed: 300, elasticBounds: 1, easing: 'easeOutExpo', dragHandle: 1, dynamicHandle: 1, clickBar: 1, prev: $section.find('.prev'), next: $section.find('.next'), prevPage: $section.find('.prevPage'), nextPage: $section.find('.nextPage'), forward: $section.find('.forward'), backward: $section.find('.backward') }); } ());
+            (function () { var $frame = $('#row2'); var $section = $frame.parent(); $frame.sly({ horizontal: 1, itemNav: 'centered', smart: 1, activateOn: 'click', mouseDragging: 1, touchDragging: 1, releaseSwing: 1, startAt: 0, scrollBar: $section.find('.scrollbar'), scrollBy: 1, pagesBar: $section.find('.pages'), activatePageOn: 'click', speed: 300, elasticBounds: 1, easing: 'easeOutExpo', dragHandle: 1, dynamicHandle: 1, clickBar: 1, prev: $section.find('.prev'), next: $section.find('.next'), prevPage: $section.find('.prevPage'), nextPage: $section.find('.nextPage'), forward: $section.find('.forward'), backward: $section.find('.backward') }); } ());
+            (function () { var $frame = $('#row3'); var $section = $frame.parent(); $frame.sly({ horizontal: 1, itemNav: 'centered', smart: 1, activateOn: 'click', mouseDragging: 1, touchDragging: 1, releaseSwing: 1, startAt: 0, scrollBar: $section.find('.scrollbar'), scrollBy: 1, pagesBar: $section.find('.pages'), activatePageOn: 'click', speed: 300, elasticBounds: 1, easing: 'easeOutExpo', dragHandle: 1, dynamicHandle: 1, clickBar: 1, prev: $section.find('.prev'), next: $section.find('.next'), prevPage: $section.find('.prevPage'), nextPage: $section.find('.nextPage'), forward: $section.find('.forward'), backward: $section.find('.backward') }); } ());
+
 
             if (screenwidth < 1000) // load tablet scripts
             {
@@ -60,26 +60,27 @@ define(['jquery', 'domReady!', 'debounced', 'lazyloader', 'easing', 'sly', 'page
                     //-- screen-size ----------------------------------
                     $("#msg").text(screenwidth + " x " + screenheight + " - desktop");
 
-                    //-- Slide to the left; if slide is model, you'll have to call $.pageslide.close() to close.
+                    //-- PageSlide ------------------------------------
+                    //-- if slide is modal, you'll have to call $.pageslide.close() to close.
                     $("a.target_blank").pageslide({ direction: "left", modal: true });
-                    $("#slidecontent").css("height", function (index)
+                    $("#slidecontent").css("height", function (index) //-- Set Height
                     {
                         var toolBar = parseInt($('#toolbar').height());
                         return screenheight - toolBar;
                     });
-
-                    //-- resize to fit -------------------------------
-                    $("#pageslide").css("width", function (index) {
+                    $("#pageslide").css("width", function (index) //-- Set Width
+                    {
                         var cover = parseInt($("div.cover").width());
                         return screenwidth - cover;
-                    });
+                        //return cover;
+                    });//-----------------------------------------------
                 });
             }
         },
         onResize: function ()
         {
             //alert(PrivateMessage());
-            
+
             // ---------------------------------------------------
             // On Smart PageResize...
             // fires only after user is done resizing the window
@@ -88,7 +89,7 @@ define(['jquery', 'domReady!', 'debounced', 'lazyloader', 'easing', 'sly', 'page
             {
                 var screenwidth = parseInt($(this).width());
                 var screenheight = parseInt($(this).height());
-                
+
                 //$(".pt-perspective").css("height", function (index)
                 //{
                 //    //var topBar = parseInt($('.topBar').height());
