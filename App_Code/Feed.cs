cf42171e20eba1140fa27e2c34f21dd6166de62c
Mo Henriquez <moses.henriquez@gmail.com>
2014-09-11 14:37:25 -0400

    updated events feed
    events on homepage are now fully dynamic

---
+++ App_Code/Feed.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+
+/// <summary>
+/// Summary description for Feed
+/// </summary>
+public class Feed
+{
+    public Feed()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+}
