4034b66c95b376097417ef89e549738d936e090a
Mo Henriquez <moses.henriquez@gmail.com>
2014-09-23 17:18:14 -0400

    styling updates

--- App_Code/Feed.cs
+++ App_Code/Feed.cs
@@ -1,16 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Web;
+using System.Xml; //+
 
 /// <summary>
 /// Summary description for Feed
 /// </summary>
 public class Feed
 {
-    public Feed()
+    private XmlDocument xmlDoc = new XmlDocument(); //-- New XML Document
+    private XmlElement xmlElement = null;
+    private XmlNodeList xmlNodeList = null;
+    
+    public Feed(string xml)
     {
-        //
-        // TODO: Add constructor logic here
-        //
+        try
+        {
+            xmlDoc.Load(xml); //-- Load Feed onto XML Doc
+	        xmlElement = xmlDoc.DocumentElement; //-- Get Root Element
+            xmlNodeList = xmlElement.SelectNodes("//channel/item"); //-- Blog Post List
+        }
+        catch
+        {
+            return;
+        }
+
+
     }
 }
